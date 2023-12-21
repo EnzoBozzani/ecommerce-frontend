8918552bc061864ac6cd88d1b7128a0eda4f7139
Enzo Bozzani <bozzanienzo@gmail.com>
2023-12-21 02:59:49 -0300

    feat: created home page wireframe and started developing it

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,12 @@
 import { ProductsService } from '@/src/services/';
+import { HeroSection } from '@/src/components/public/HeroSection';
 
 export default async function Home() {
-	const { products, page, perPage, total, message } = await ProductsService.getFeaturedProducts();
-
-	if (message) return <span>{message}</span>;
+	const { products, page, perPage, total } = await ProductsService.getFeaturedProducts();
 
 	return (
-		<main className='flex w-100 justify-around flex-wrap mt-36 gap-12'>
-			<div className='w-72 h-72 bg-primary'>{page}</div>
-			<div className='w-72 h-72 bg-primaryLight'>{perPage}</div>
-			<div className='w-72 h-72 bg-sec'>{total}</div>
-			<div className='w-72 h-72 bg-light'>
-				{products.map((product: any) => (
-					<span key={product.id}>{product.name}</span>
-				))}
-			</div>
-			<div className='w-72 h-72 bg-white/25'></div>
-			<div className='text-white/25'></div>
+		<main className='w-full min-h-screen bg-dark pt-12'>
+			<HeroSection />
 		</main>
 	);
 }
---
+++ src/components/public/HeroSection.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import { Logo } from '..';
+import Link from 'next/link';
+
+export const HeroSection: FC = () => {
+	return (
+		<section className='w-11/12 bg-primary flex flex-col p-8 mx-auto max-w-[1200px]'>
+			<div className='w-full flex'>
+				<div className='flex gap-4 items-center'>
+					<Logo className='w-12 h-12 text-light' />
+					<h3 className='text-xl sm:text-2xl text-light'>Logo</h3>
+				</div>
+			</div>
+			<div className='w-full pt-12 flex flex-col items-center justify-center gap-4'>
+				<h1 className='text-light text-2xl sm:text-4xl font-bold text-center w-2/3'>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, repellat.
+				</h1>
+				<h5 className='text-dark text-center w-2/3'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ab quam, numquam et tenetur vero
+					molestiae dolorem accusantium assumenda porro?
+				</h5>
+				<Link href={'/register'}>
+					<button className='personalizedButton text-light mt-12 font-bold'>Faça Parte</button>
+				</Link>
+			</div>
+		</section>
+	);
+};
