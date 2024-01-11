9cc43883eb5f46c2dfb42016aafe132ca56ee029
Enzo Bozzani <bozzanienzo@gmail.com>
2024-01-11 03:18:11 -0300

    feat: created service to consume new endpoint

--- src/components/public/ProductArea.tsx
+++ src/components/public/ProductArea.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { Product } from '@/app/[id]/page';
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Loader } from '..';
+import FavoritesService from '@/src/services/FavoritesService';
 
 interface Props {
 	product: Product | undefined;
@@ -12,11 +14,39 @@ interface Props {
 
 export const ProductArea: FC<Props> = ({ product, width }) => {
 	const [selectedImage, setSelectedImage] = useState(0);
+	const [isFavorited, setIsFavorited] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchIsFavorited = async () => {
+			const res = await FavoritesService.isProductFavorited(+product!.id);
+			if (res.favorited) {
+				setIsFavorited(true);
+				return setIsLoading(false);
+			}
+			setIsFavorited(false);
+			setIsLoading(false);
+		};
+		fetchIsFavorited();
+	}, []);
+
+	const handleClickToFavoriteProduct = async () => {
+		setIsLoading(true);
+
+		location.reload();
+	};
+
+	if (isLoading)
+		return (
+			<div className='w-full min-h-screen flex justify-center items-center bg-dark'>
+				<Loader />
+			</div>
+		);
 
 	return (
 		<>
 			<section className='mx-auto max-w-screen-xl flex flex-col lg:flex-row pt-16 px-6'>
-				{width < 1024 && <h1 className='pb-12 text-4xl lg:text-5xl text-justify'>{product!.name}</h1>}
+				{width < 1024 && <h1 className='pb-12 text-3xl lg:text-5xl text-justify'>{product!.name}</h1>}
 				<div className='w-full lg:w-1/2 lg:h-[500px]'>
 					<div className='lg:h-full w-full flex flex-col gap-8 justify-center items-center'>
 						<div className='lg:w-full flex items-center'>
@@ -73,12 +103,12 @@ export const ProductArea: FC<Props> = ({ product, width }) => {
 						<div className='flex items-center gap-4'>
 							{product!.images.map((src, i) => (
 								<div
+									key={i}
 									className={`cursor-pointer w-[50px] h-[50px] flex justify-center items-center ${
 										i === selectedImage ? 'border-2 border-primaryLight' : ''
 									}`}
 								>
 									<Image
-										key={i}
 										alt={`${product!.name} image ${i + 1}`}
 										src={`http://${src}`}
 										width={50}
@@ -94,7 +124,7 @@ export const ProductArea: FC<Props> = ({ product, width }) => {
 					{width >= 1024 && <h1 className='text-4xl lg:text-5xl text-justify'>{product!.name}</h1>}
 					<div className='w-full flex flex-col gap-4 lg:gap-8'>
 						<div className='rounded-xl border border-dark p-6 flex flex-col'>
-							<h3 className='text-3xl md:text-4xl'>
+							<h3 className='text-xl md:text-4xl'>
 								{product!.price.toLocaleString('pt-BR', {
 									style: 'currency',
 									currency: 'BRL',
@@ -103,31 +133,55 @@ export const ProductArea: FC<Props> = ({ product, width }) => {
 							</h3>
 						</div>
 						<Link href={`/purchase/${product!.id}`}>
-							<button className='w-full border rounded-xl px-4 py-6 bg-primaryLight text-light text-3xl'>
+							<button className='w-full border rounded-xl px-2 py-3 xs:px-4 xs:py-6 bg-primaryLight text-light text-xl xs:text-3xl'>
 								Comprar
 							</button>
 						</Link>
 					</div>
 				</div>
 			</section>
-			<div className='mx-auto px-4 xs:px-12 pb-36 lg:pb-24 max-w-screen-xl flex flex-col gap-8'>
+			<div className='mx-auto px-6 lg:pt-12 pb-36 lg:pb-24 max-w-screen-xl flex flex-col gap-8'>
 				<h1 className='text-4xl lg:text-5xl text-justify'>Descrição</h1>
 				<p className='text-justify'>{product!.description}</p>
-				<button className='rounded text-light mx-auto w-[150px] border border-light bg-yellow-500 px-4 py-2 flex justify-center items-center gap-2'>
-					<p>Favoritar</p>
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						viewBox='0 0 24 24'
-						fill='currentColor'
-						className='w-6 h-6'
+				{isFavorited ? (
+					<button
+						onClick={() => handleClickToFavoriteProduct()}
+						className='rounded text-light mx-auto w-[150px] border border-light bg-red-500 px-4 py-2 flex justify-center items-center gap-2'
 					>
-						<path
-							fillRule='evenodd'
-							d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z'
-							clipRule='evenodd'
-						/>
-					</svg>
-				</button>
+						<p>Desfavoritar</p>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							viewBox='0 0 24 24'
+							fill='currentColor'
+							className='w-6 h-6'
+						>
+							<path
+								fillRule='evenodd'
+								d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z'
+								clipRule='evenodd'
+							/>
+						</svg>
+					</button>
+				) : (
+					<button
+						onClick={() => handleClickToFavoriteProduct()}
+						className='rounded text-light mx-auto w-[150px] border border-light bg-yellow-500 px-4 py-2 flex justify-center items-center gap-2'
+					>
+						<p>Favoritar</p>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							viewBox='0 0 24 24'
+							fill='currentColor'
+							className='w-6 h-6'
+						>
+							<path
+								fillRule='evenodd'
+								d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z'
+								clipRule='evenodd'
+							/>
+						</svg>
+					</button>
+				)}
 			</div>
 		</>
 	);
---
+++ src/services/FavoritesService.ts
@@ -0,0 +1,32 @@
+export default class FavoritesService {
+	static async saveFavorite(productId: number) {
+		const token = localStorage.getItem('ecommerce-token');
+
+		const res = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/favorites`, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify({ productId }),
+		});
+
+		return res.json();
+	}
+
+	static async isProductFavorited(productId: number) {
+		const token = localStorage.getItem('ecommerce-token');
+
+		const res = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/isFavorited?id=${productId}`, {
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		return res.json();
+	}
+}
